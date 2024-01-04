b79b73b7fbe806f739486fdd6373e1bf11b91633
DraghettiGiacomo <jack.draghetti2002@gmail.com>
2024-01-04 22:36:25 +0100

    add works section, but there are some problems with js title animation

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,7 +2,7 @@
 /* ---------*************** ANIMATION TITLE ***************--------- */
 
 let posizioneFinale = 18;
-let incremento = 2;
+let incremento = 0;
 
 window.addEventListener('scroll', function() {
     if(window.pageYOffset > 400){
@@ -13,9 +13,10 @@ window.addEventListener('scroll', function() {
 function animationTitle(){
     if(incremento > posizioneFinale){
         console.log('finito');
+        incremento = 0;
         Session.Contents.Remove( incremento )
-        document.getElementById('works').style.margin = '0 auto'
-        document.getElementById('works').style.left = '0'
+        /* document.getElementById('works').style.margin = '0 auto'
+        document.getElementById('works').style.left = '0' */
     } else {
         document.getElementById('works').style.left = incremento + '%'
         incremento += 2
