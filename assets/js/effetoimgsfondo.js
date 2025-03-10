4c8f3d78549363255b94a1959afdedcf6b7d9883
DraghettiGiacomo <jack.draghetti2002@gmail.com>
2025-03-10 22:03:55 +0100

    animazioni carine

---
+++ assets/js/effetoimgsfondo.js
@@ -0,0 +1,15 @@
+/* let immagineSfondo = document.getElementById("provaeffettovetro")
+
+
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
+ */
+/* window.addEventListener("scroll", () => {
+    immagineSfondo.style.width = "80%"
+    immagineSfondo.style.transition = "1s"
+}); */
+/* window.onscroll = function() {
+    immagineSfondo.style.width = "80%"
+    immagineSfondo.style.transition = "1s"
+} */
